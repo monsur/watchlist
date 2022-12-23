41f8748c5dda01a466725bd95a1f7baddc43b658
Monsur Hossain <monsur@gmail.com>
2022-12-23 15:24:15 -0600

    Added fixed id to the json

--- src/Grid.js
+++ src/Grid.js
@@ -2,18 +2,9 @@ import './Grid.css';
 import GridItem from './GridItem';
 
 function Grid(props) {
-
-  function getKey(item) {
-    let key = item.brand + ' ' + item.collection;
-    if (item.detail) {
-      key += ' ' + item.detail;
-    }
-    return key;
-  }
-
   return (
     <div className="Grid">
-    {props.data.map((item) => <GridItem key={getKey(item)} item={item} />)}
+    {props.data.map((item) => <GridItem key={item.id} item={item} />)}
     </div>
   );
 }
