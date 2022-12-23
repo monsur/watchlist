c20d9681eac11d28e93f870a95402b1bd1a25e43
Monsur Hossain <monsur@gmail.com>
2022-12-23 14:46:41 -0600

    Adding default sort

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
 import data from './data.json';
 import './App.css';
 import Grid from './Grid';
 
 function App() {
+  let getSortFunction = function() {
+    return function(a, b) {
+      return parseFloat(a.price) - parseFloat(b.price);
+    };
+  };
+
+  data.sort(getSortFunction());
+
   return (
     <div className="App">
       <Grid data={data} />
