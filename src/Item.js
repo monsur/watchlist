1f50ea98e0a6970d732e95dd276b820307a74cbc
Monsur Hossain <monsur@gmail.com>
2022-12-24 17:50:50 -0600

    Adding a menu to item

--- src/Item.js
+++ src/Item.js
@@ -3,6 +3,11 @@ import data from "./data.json";
 import { useParams } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Helpers from "./helpers.js";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 function Item() {
   const { itemId } = useParams();
@@ -31,6 +36,24 @@ function Item() {
 
   return (
     <Box>
+      <Box sx={{ flexGrow: 1, marginBottom: "100px" }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <ArrowBackIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              My Watch Wishlist
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <div>
         <img src={Helpers.getImageUrl(item.image)}></img>
       </div>
