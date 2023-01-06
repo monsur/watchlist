37cd968ddadb88380dcb54b309928ed8be98a9fe
Monsur Hossain <monsur@gmail.com>
2023-01-06 00:34:14 -0600

    Adding new types.

---
+++ src/Types.ts
@@ -0,0 +1,19 @@
+export interface WatchData {
+  id: string;
+  brand: string;
+  collection: string;
+  detail: string;
+  type: string;
+  image: string;
+  link: string;
+  price: number;
+  ref: string;
+  movement: string;
+  frequency: number;
+  diameter: number;
+  thickness: number;
+  water_resistance: number;
+  crystal: string;
+  strap: string;
+  color: string[];
+}
