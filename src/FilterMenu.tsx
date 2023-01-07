094bb16a4d10f4dce8a7c7807ce3c832ccde8509
Monsur Hossain <monsur@gmail.com>
2023-01-07 11:40:40 -0600

    Displaying brands in filter menu.

--- src/FilterMenu.tsx
+++ src/FilterMenu.tsx
@@ -1,7 +1,15 @@
 import FilterOptions from "./FilterOptions";
+import Checkbox from "@mui/material/Checkbox";
+import { FormControlLabel, FormGroup } from "@mui/material";
 
 function FilterMenu(props: { filterOptions: FilterOptions }) {
-  return <div>TODO</div>;
+  return (
+    <FormGroup>
+      {props.filterOptions.brands.map(({ key, count }, index) => {
+        return (<FormControlLabel control={<Checkbox />} label={`${key} (${count})`} key={key} />);
+      })}
+    </FormGroup>
+  );
 }
 
 export default FilterMenu;
