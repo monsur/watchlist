d159dc6c38359afce9c244bd6810984bf73c4568
Monsur Hossain <monsur@gmail.com>
2023-01-07 11:44:54 -0600

    Adding a button.

--- src/FilterMenu.tsx
+++ src/FilterMenu.tsx
@@ -1,14 +1,24 @@
 import FilterOptions from "./FilterOptions";
 import Checkbox from "@mui/material/Checkbox";
-import { FormControlLabel, FormGroup } from "@mui/material";
+import { Box, Button, FormControlLabel, FormGroup } from "@mui/material";
 
 function FilterMenu(props: { filterOptions: FilterOptions }) {
   return (
-    <FormGroup>
-      {props.filterOptions.brands.map(({ key, count }, index) => {
-        return (<FormControlLabel control={<Checkbox />} label={`${key} (${count})`} key={key} />);
-      })}
-    </FormGroup>
+    <Box>
+      <Button variant="outlined">Clear</Button>
+      <Button variant="contained">Apply</Button>
+      <FormGroup>
+        {props.filterOptions.brands.map(({ key, count }, index) => {
+          return (
+            <FormControlLabel
+              control={<Checkbox />}
+              label={`${key} (${count})`}
+              key={key}
+            />
+          );
+        })}
+      </FormGroup>
+    </Box>
   );
 }
 
